0055318d769b671315ffb45115a7d4cbb1ff7f46
AmitxParmar <amitparmar901@gmail.com>
2023-01-09 23:59:50 +0530

    create schema

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
-
+const connection = require('./db');
+const cors = require('cors');
 const port = process.env.PORT || 8080;
 
+
+connection();
 app.use(express.json());
 app.use(cors());
 
