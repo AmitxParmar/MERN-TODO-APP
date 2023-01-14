8c575017f5adc764b3a24c3eb80bb689f5526544
AmitxParmar <amitparmar901@gmail.com>
2023-01-14 23:35:03 +0530

    removed es6 refactoring add: get and post method in api and test it.

--- Server/index.js
+++ Server/index.js
@@ -1,25 +1,49 @@
-import cors from 'cors';
-import express, { json } from 'express';
-import { connect, set } from 'mongoose';
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(json());
+app.use(express.json());
 app.use(cors());
 
 try {
-    set("strictQuery", false);
-
-    connect(//address here mongo
+    mongoose.set("strictQuery", false);
+    mongoose.connect(//address here mongo
         "mongodb://localhost:27017/mern-todo",
         //process.env.MONGO_URL,
         () => console.log('Connected to MongoDB')
     );
-
 } catch (err) {
     console.log("Couldn't connect to MongoDB ", err);
 }
+
+const Todo = require('./models/Todo');
+
+app.get('/todos', async (req, res) => {
+    const todos = await Todo.find();
+    res.json(todos);
+});
+
+
+app.post('/todo/new', (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
+    });
+    todo.save();
+    res.json(todo);
+});
+
+/* app.delete('/todos/delete/:id', async (req, res) => {
+    const result = await Todo.findByIdAndDelete(req.params.id);
+    res.json(result);
+}); */
+
+/* app.patch('', (req, res) => { }); */
+
+
+
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
--- Server/models/Todo.js
+++ Server/models/Todo.js
@@ -1,16 +1,21 @@
 const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
 
-const taskSchema = new Schema({
-    task: {
+const TodoSchema = new Schema({
+    text: {
         type: String,
         required: true
     },
-    completed: {
+    complete: {
         type: Boolean,
         default: false
+    },
+    timestamp: {
+        type: String,
+        default: Date.now()
     }
 });
 
-const Todo = mongoose.model("task", taskSchema);
+const Todo = mongoose.model("Todo", TodoSchema);
 module.exports = Todo;
