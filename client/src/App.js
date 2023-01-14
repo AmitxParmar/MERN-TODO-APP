c76e9c33b606a6505190643e913085ab060439c6
AmitxParmar <amitparmar901@gmail.com>
2023-01-15 02:34:23 +0530

    add styles

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,21 @@ function App() {
       <h1>Welcome, {user}</h1>
       <h4>Your Tasks</h4>
 
-      <div className='todos'></div>
+      <div className='todos'>
+        <div className='todo'>
+          <div className='checkbox'></div>
+
+          <div className="text">Get the Bread</div>
+
+          <div className="delete-todo"></div>
+
+        </div>
+        <div className="todo">
+          <div className="checkbox"></div>
+          <div className="text">get</div>
+          <div className="delete-todo">x</div>
+        </div>
+      </div>
     </div>
   );
 }
